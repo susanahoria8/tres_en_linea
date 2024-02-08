00a5149b1ee5848044a499758dbfab887a99e242
susanahoria8 <susanaromero040800@gmail.com>
2024-02-08 15:19:03 +0100

    tres en linea v 2.0

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Game from './Game';
+
+function App() {
+
+  return (
+    <> 
+      <div>
+        <h1>Tres En Linea</h1>
+        <Game /> 
+      </div> 
+    </>
+  )
+}
+
+export default App;
---
+++ src/Square.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './Square.css';
+
+function Square ({value, onSquareClick}) {
+	
+
+	return(
+			<>
+				<button className="square" onClick={onSquareClick}>
+				{value}
+				</button>
+			</>
+		);
+}
+
+export default Square;
